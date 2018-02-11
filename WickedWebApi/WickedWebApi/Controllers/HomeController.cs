599adb849828561f007ad191425ecb0ad088b4b6
DESKTOP-FA62B5E\Ferret <sefannemi16@gmail.com>
2018-02-11 17:46:31 +0200

    Entity Framework ¯\_(ツ)_/¯ <3

--- WickedWebApi/WickedWebApi/Controllers/HomeController.cs
+++ WickedWebApi/WickedWebApi/Controllers/HomeController.cs
@@ -1,18 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Configuration;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using SqlServerConnection;
+using WickedWebApi.DAL;
 
 namespace WickedWebApi.Controllers
 {
-   public class HomeController : Controller
-   {
-      public ActionResult Index()
-      {
-         ViewBag.Title = "Home Page";
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
 
-         return View();
-      }
-   }
+            return View();
+        }
+        [HttpPost]
+        public string GetAll()
+        {
+            LoginContext loginContext = new LoginContext();
+            
+            return loginContext.Students.Select(st => st.ToString()).Aggregate((st1, st2) => st1 + st2);
+        }
+
+        [HttpPost]
+        public string FindByName(string name)
+        {
+            LoginContext loginContext = new LoginContext();
+            string result = loginContext.Students.Find(int.Parse(name))?.ToString();
+
+            return result;
+        }
+    }
 }
