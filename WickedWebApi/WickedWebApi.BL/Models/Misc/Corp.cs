4f5fa861e40f2e09fc8a051855c19db31502bdd4
DESKTOP-FA62B5E\Ferret <sefannemi16@gmail.com>
2018-02-12 22:05:26 +0200

    Revert "Revert "Merge branch 'Dev-Stefan'""
    This reverts commit 3eb6d73b9eae7dcbfa4e2d740a8e3fed06994fb8.

---
+++ WickedWebApi/WickedWebApi.BL/Models/Misc/Corp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WickedWebApi.BL.Models.Misc
+{
+    public class Corp
+    {
+        public int Id { get; set; }
+        public string Denumire { get; set; }
+        public List<string> Sali { get; set; }
+    }
+}
