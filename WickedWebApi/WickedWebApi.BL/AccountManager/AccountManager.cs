d296c5a2ac67f542ff31279c060d11f692e881f8
TeodoraMaria <teodoramaria0112@gmail.com>
2018-02-12 21:15:05 +0200

    arhitecture stuff

---
+++ WickedWebApi/WickedWebApi.BL/AccountManager/AccountManager.cs
@@ -0,0 +1,16 @@
+﻿
+
+using WickedWebApi.DAL;
+
+namespace WickedWebApi.BL.AccountManager
+{
+   public class AccountManager : IAccountManager
+   {
+      public bool CheckEmail(string email)
+      {
+         LoginContext lc = new LoginContext();
+         lc.Accounts.
+         
+      }
+   }
+}
---
+++ WickedWebApi/WickedWebApi/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SqlServerConnection;
+using WickedWebApi.DAL;
+
+namespace WickedWebApi.Controllers
+{
+   public class AccountController : Controller
+   {
+
+      [HttpPost]
+      public bool CheckEmail(string email)
+      {
+
+      }
+
+
+   }
+}
